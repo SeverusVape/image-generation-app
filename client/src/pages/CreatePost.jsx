9fd0f7815f11c13217b8c7356901f77434eb2f57
Veter v Golove <koshelevworkmail@gmail.com>
2023-01-21 18:31:12 -0500

    server openAi response check

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -25,7 +25,39 @@ const CreatePost = () => {
         setForm({ ...form, prompt: randomPrompt });
     };
 
-    const generateImage = () => {};
+    const generateImage = async () => {
+        if (form.prompt) {
+            try {
+                setGeneratingImg(true);
+                const response = await fetch(
+                    "http://localhost:8080/api/v1/dalle",
+                    {
+                        method: "POST",
+                        headers: {
+                            // prettier-ignore
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            prompt: form.prompt,
+                        }),
+                    }
+                );
+
+                const data = await response.json();
+
+                setForm({
+                    ...form,
+                    photo: `data:image/jpeg;base64,${data.photo}`,
+                });
+            } catch (err) {
+                alert(err);
+            } finally {
+                setGeneratingImg(false);
+            }
+        } else {
+            alert("Please provide proper prompt");
+        }
+    };
 
     return (
         <section className="max-w-7xl mx-auto">
